fbe3eeca8bc3e59a062697d50a142bcad9759b9e
Ny Hasina Vagno <hei.nyhasina@gmail.com>
2026-07-28 17:16:39 +0200

    chore: authService IT

--- src/main/java/app/esiroi/auth/model/dto/MfaChallenge.java
+++ src/main/java/app/esiroi/auth/model/dto/MfaChallenge.java
@@ -2,9 +2,13 @@ package app.esiroi.auth.model.dto;
 
 import java.io.Serializable;
 import java.time.Instant;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MfaChallenge implements Serializable {
   private String challengeId;
   private String userId;
--- src/test/java/app/esiroi/auth/integration/service/AuthServiceIT.java
+++ src/test/java/app/esiroi/auth/integration/service/AuthServiceIT.java
@@ -1,25 +1,38 @@
 package app.esiroi.auth.integration.service;
 
 import static java.util.UUID.randomUUID;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 import app.esiroi.auth.ITestConfiguration;
+import app.esiroi.auth.endpoint.rest.model.AuthUser;
+import app.esiroi.auth.endpoint.security.TOTPConf;
 import app.esiroi.auth.model.User;
+import app.esiroi.auth.model.dto.MfaChallenge;
+import app.esiroi.auth.model.exception.ForbiddenException;
 import app.esiroi.auth.model.exception.NotFoundException;
 import app.esiroi.auth.repository.UserRepository;
 import app.esiroi.auth.service.AuthService;
+import app.esiroi.auth.service.MfaChallengeService;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 class AuthServiceIT extends ITestConfiguration {
   @Autowired UserRepository userRepository;
   @Autowired AuthService subject;
+  @MockBean MfaChallengeService mfaChallengeService;
+  @MockBean TOTPConf totpConf;
 
   @BeforeEach
   void setUp() {
     userRepository.save(expected());
+    when(mfaChallengeService.create(anyString())).thenReturn(challenge());
+    when(mfaChallengeService.get(anyString())).thenReturn(Optional.of(challenge()));
+    when(totpConf.validateTOTP(anyString(), anyString())).thenReturn(true);
   }
 
   @Test
@@ -49,11 +62,41 @@ class AuthServiceIT extends ITestConfiguration {
     assertThrows(NotFoundException.class, () -> subject.getUserById(id));
   }
 
+  @Test
+  void authenticate_user_ok() {
+    var toAuthenticate = new AuthUser().email(expected().getEmail()).password("test");
+
+    var actual = subject.authenticateUser(toAuthenticate);
+
+    assertEquals(challenge(), actual);
+  }
+
+  @Test
+  void authenticate_user_ko() {
+    var toAuthenticate = new AuthUser().email(expected().getEmail()).password("wrongpassword");
+
+    assertThrows(ForbiddenException.class, () -> subject.authenticateUser(toAuthenticate));
+  }
+
+  @Test
+  void validate_otp_ok() {
+    String otp = "123456";
+
+    var actual = subject.validateOTP(challenge().getChallengeId(), otp);
+
+    assertNotNull(actual.getAccessToken());
+  }
+
   private User expected() {
     return User.builder()
         .id("user1_id")
         .email("test@email.com")
-        .passwordHash("967520ae23e8ee14888bae72809031b98398ae4a636773e18fff917d77679334")
+        .passwordHash("$2a$12$7EAKNtczSkndY4JHkdW9XeLTD3akaoi4Uqmk8wrqfxZF7of8F4kom")
+        .otpSecret("*".repeat(16).getBytes())
         .build();
+  }
+
+  private MfaChallenge challenge() {
+    return new MfaChallenge("challengeId", expected().getId(), 1, null);
   }
 }
